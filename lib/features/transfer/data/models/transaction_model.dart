f432fc9f2671db5469f2d19215a0d671285928cf
gromx-log <filbertferdinand270@gmail.com>
2025-09-23 10:42:34 +0700

    I did some slight change in transaction_model, and transfer repository Transaction_model now saves senderName and senderAccount (us), and also made it more "rapih"
    recipient_model now saves an id, bankcode and bankLogo
    
    Added a new bank_model, to save the name, code, and logo of the bank
    
    Lastly added repository as the base, for the cubits, and added some dummy data (there's an easteregg inside of it lmao)

--- lib/features/transfer/data/models/transaction_model.dart
+++ lib/features/transfer/data/models/transaction_model.dart
@@ -1,31 +1,42 @@
 // Model ini akan digunakan untuk transaksi kita
 enum TransactionStatus { success, failed, pending }
-enum TransactionType { antarBank, antarRekening, va }
+enum TransactionType { antarBank, antarRekening, virtualAccount }
 
 class TransactionModel {
   // Berikut adalah data yang wajib data
   final String id;
   final double amount;
+  final DateTime timestamp;
+  final TransactionStatus status; //success/failed/pending
+  final TransactionType type; //Simpan informasi apakah transaksi ini termasuk transaksi
+                              // antar-rekening, antar-bank, atau lewat virtual account
+  
+  // Sisi pengirim
+  final String senderName;
+  final String senderAccount;
+
+  // Sisa penerima
   final String recipientName;
   final String recipientAccount;
-  final DateTime timestamp;
-  final TransactionStatus status;
-  final TransactionType type;
+  final String? recipientBankName; //Untuk simpan nama bank (semisalnya menggunakan transfer antar-bank/VA)
 
-  // Adapun beberapa data yang bersifat opsional
+  // Adapun data lain yang sifatnya opsional, sebagai berikut:
   final String? notes; //Catatan transaksi
-  final String? bankName; //Nama dari sebuah bank (tidak wajib untuk transfer antar-rekening)
+  
   TransactionModel({
-    required this.id,
-    required this.amount,
-    required this.recipientName,
-    required this.recipientAccount,
-    required this.timestamp,
-    required this.status,
-    required this.type,
-
-    // Juga untuk data opsional
-    this.notes,
-    this.bankName,
+  required this.id,
+  required this.amount,
+  required this.timestamp,
+  required this.status,
+  required this.type,
+  // Pengirim
+  required this.senderName,
+  required this.senderAccount,
+  // Penerima
+  required this.recipientName,
+  required this.recipientAccount,
+  // Data opsional
+  this.recipientBankName,
+  this.notes,
   });
 }
---
+++ lib/features/transfer/data/repositories/transfer_repository.dart
@@ -0,0 +1,107 @@
+import 'package:vaultbank/features/transfer/data/models/bank_model.dart';
+import 'package:vaultbank/features/transfer/data/models/recipient_model.dart';
+import 'package:vaultbank/features/transfer/data/models/transaction_model.dart';
+
+abstract class TransferRepository {
+  // List daftar bank yang tersedia
+  Future<List<BankModel>> getBankList();
+
+  // List daftar penerima yang disimpan
+  Future<List<RecipientModel>> getSavedRecipients();
+
+  // Riwayat transaksinya
+  Future<List<TransactionModel>> getTransactionHistory();
+}
+
+// Nah untuk mendukung development
+// Sambil menunggu pematangan UI saya izin inisiatif membuat beberapa data dummy
+class FakeTransferRepository implements TransferRepository{
+  // Bagian ini jadi list untuk nama bank palsunya  
+  // Saya gunakan nama anggota saya sendiri sebagai easter egg
+  final List<BankModel> _dummyBanks = [
+    BankModel(name: "Erick National Bank", code: "010"),
+    BankModel(name: "Gerald's Priority", code: "030"),
+    BankModel(name: "Anstendyk", code: "130"),
+    BankModel(name: "New Ferdinand Central", code: "135"),
+    BankModel(name: "The NG", code: "153"),
+  ];
+
+  final List<RecipientModel> _dummyRecipients = [
+    RecipientModel(id: "1", name: 'Coach Mariano', accountNumber: '4133313331', bankName: "New Ferdinand Central", bankCode: "135"),
+    RecipientModel(id: "2", name: "Sulistyo Luis", accountNumber: '1313321020', bankName: 'The NG', bankCode: "153"),
+  ];
+
+  List<TransactionModel> _dummyTransactions = [
+    TransactionModel(
+      id: 'tx1', amount: 50000, timestamp: DateTime.now().subtract(const Duration(days: 1)),
+      type: TransactionType.antarBank, status: TransactionStatus.success,
+      senderName: 'Filbert Ferdinand', senderAccount: '123456789',
+      recipientName: 'Budi Hartono', recipientAccount: '1122334455', recipientBankName: 'Gerald\'s Priority',
+    ),
+    TransactionModel(
+      id: 'tx2', amount: 125000, timestamp: DateTime.now().subtract(const Duration(days: 3)),
+      type: TransactionType.virtualAccount, status: TransactionStatus.success,
+      senderName: 'Filbert Ferdinand', senderAccount: '123456789',
+      recipientName: 'Toko Online', recipientAccount: '88001122334455', recipientBankName: 'Anstendyk'
+    ),
+  ];
+
+  // Terapkan fungsi-fungsi yang sudah di set di dalam abstract class TransferRepository
+  // Ambil list bank
+  @override
+  Future<List<BankModel>> getBankList() async {
+    await Future.delayed(const Duration(milliseconds: 800));
+    return _dummyBanks;
+  }
+  // Ambil list penerima yang sudah kita simpan
+  @override
+  Future<List<RecipientModel>> getSavedRecipients() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    return _dummyRecipients;
+  }
+
+  // Ambil transaction history kita
+  @override
+  Future<List<TransactionModel>> getTransactionHistory() async {
+    await Future.delayed(const Duration(milliseconds: 1200));
+    // Urutkan agar transaksi ditampilkan mulai dari yang terbaru ke yang terlama
+    _dummyTransactions.sort((a, b) => b.timestamp.compareTo(a.timestamp));
+    return _dummyTransactions;
+  }
+
+  // Lakukan transfer
+  @override
+  Future<TransactionModel> performTransfer({
+    required double amount,
+    required RecipientModel recipient,
+    required String notes,
+  }) async {
+    // Simulasi jika ada delay transfer selama 2 detik
+    await Future.delayed(const Duration(seconds: 2));
+
+    // Simulasi bila ada kegagalan untuk melakukan transfer
+    if (recipient.accountNumber == '0000000000') {
+      throw Exception("Rekening tujuan diblokir.");
+    }
+
+    // Buat objek baru, jika berhasil melakukan transaksi
+    final newTransaction = TransactionModel(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      amount: amount,
+      timestamp: DateTime.now(),
+      type: TransactionType.antarBank,
+      status: TransactionStatus.success,
+      senderName: 'Filbert Ferdinand', // JANGAN LUPA DI GANTI DENGAN NAMA USER ASLI
+      senderAccount: '123456789',      // JANGAN LUPA DI GANTI DENGAN NO AKUN USER ASLI
+      recipientName: recipient.name,
+      recipientAccount: recipient.accountNumber,
+      recipientBankName: recipient.bankName,
+      notes: notes,
+    );
+
+    // Terakhir simpan hasil transaksi baru tersebut ke dalam daftar riwayat
+    _dummyTransactions.add(newTransaction);
+    
+    return newTransaction;
+  }
+}
